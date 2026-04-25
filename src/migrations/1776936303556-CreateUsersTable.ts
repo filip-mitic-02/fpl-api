5329d888118e7581f3adc9a05f6eda5f065567e1
Filip Mitic <philippos.scorpion02@gmail.com>
2026-04-25 21:13:39 +0200

    Added CreateUserTable migration

---
+++ src/migrations/1776936303556-CreateUsersTable.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUsersTable1776936303556 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+                name varchar(255) NOT NULL,
+                surname varchar(255) NOT NULL,
+                email varchar(255) NOT NULL,
+                username varchar(255) NOT NULL,
+                password varchar(255) NOT NULL,
+                "dateOfBirth" date NOT NULL,
+                "createdAt" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                "updatedAt" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                "deletedAt" timestamp NULL 
+            );
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE IF EXISTS users
+        `)
+    }
+
+}
