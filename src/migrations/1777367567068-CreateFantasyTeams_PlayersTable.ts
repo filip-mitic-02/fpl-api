a0ae70ebd3a3a45bc76fef9d72f61c2a1a7f1f66
Filip Mitic <philippos.scorpion02@gmail.com>
2026-04-28 11:24:49 +0200

    Added CreateFantasyTemas_PlayersTable migration

---
+++ src/migrations/1777367567068-CreateFantasyTeams_PlayersTable.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateFantasyTeamsPlayersTable1777367567068 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS "fantasyTeams_Players"(
+                "fantasyTeamId" uuid NOT NULL,
+                "playerId" uuid NOT NULL,
+                "isCaptain" boolean NOT NULL DEFAULT false,
+                "isViceCaptain" boolean NOT NULL DEFAULT false,
+                "onBench" boolean NOT NULL DEFAULT false,
+
+                PRIMARY KEY("fantasyTeamId", "playerId"),
+
+                CONSTRAINT "fk_fantasyTeamsPlayers_fantasyTeams"
+                    FOREIGN KEY("fantasyTeamId")
+                    REFERENCES "fantasyTeams"(id),
+
+                CONSTRAINT "fk_fantasyTeamsPlayers_players"
+                    FOREIGN KEY("playerId")
+                    REFERENCES players(id)
+            ); 
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE IF EXISTS "fantasyTeams_Players"    
+        `);
+    }
+
+}
