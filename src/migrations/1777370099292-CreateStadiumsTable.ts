d197da50d7cc62a1b5f9bafa5155ff75177dd206
Filip Mitic <philippos.scorpion02@gmail.com>
2026-04-28 12:13:18 +0200

    Added CreateStadiumsTable migration as well as CreateGameweeksTable and pivot table migrations related to it

---
+++ src/migrations/1777370099292-CreateStadiumsTable.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateStadiumsTable1777370099292 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS stadiums(
+                id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+                name varchar(100) NOT NULL  
+            );    
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE IF EXISTS stadiums    
+        `);
+    }
+
+}
---
+++ src/migrations/1777370361863-CreateGameweeks_ClubsTable.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateGameweeksClubsTable1777370361863 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS "gameweeks_Clubs"(
+                "gameweekId" uuid NOT NULL,
+                "clubId" uuid NOT NULL,
+                "stadiumId" uuid NOT NULL,
+                date date NOT NULL,
+
+                PRIMARY KEY("gameweekId", "clubId"),
+
+                CONSTRAINT "fk_gameweeksClubs_gameweeks"
+                    FOREIGN KEY("gameweekId")
+                    REFERENCES gameweeks(id),
+
+                CONSTRAINT "fk_gameweeksClubs_clubs"
+                    FOREIGN KEY("clubId")
+                    REFERENCES clubs(id),
+
+                CONSTRAINT "fk_gameweeksClubs_stadiums"
+                    FOREIGN KEY("stadiumId")
+                    REFERENCES stadiums(id)
+            );    
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE IF EXISTS "gameweeks_Clubs"    
+        `);
+    }
+
+}
