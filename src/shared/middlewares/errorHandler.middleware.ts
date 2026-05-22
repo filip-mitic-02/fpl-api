eaa12449ab040dc3e53a2df55b16a5a82779f235
Filip Mitic <philippos.scorpion02@gmail.com>
2026-05-22 11:15:57 +0200

    Added logger

--- src/shared/middlewares/errorHandler.middleware.ts
+++ src/shared/middlewares/errorHandler.middleware.ts
@@ -2,11 +2,16 @@ import { Request, Response, NextFunction } from 'express';
 import { BadRequestException, HttpException } from '../exceptions';
 import { sendResponse } from '../responses';
 import { StatusCode } from '../enums';
+import { logger } from '../utils';
 
 export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
   const statusCode = error instanceof HttpException ? error.statusCode : StatusCode.SERVER_ERROR;
   const message = error instanceof HttpException ? error.message : 'Server error.';
   const errors = error instanceof BadRequestException ? error.errors : undefined;
+
+  if (!(error instanceof HttpException)) {
+    logger.error('Unexpected error occured.', { message: error.message, trace: error.stack });
+  }
 
   return sendResponse(res, statusCode, message, undefined, errors);
 };
